0413ce18b6df4f3b846da9ab18fb994f7b38359d
DevolperPHP <devolperphp@gmail.com>
2020-02-12 05:43:10 -0800

    Update for.js

--- src/for.js
+++ src/for.js
@@ -1,1 +1,11 @@
+// NODE JS
 
+a = [1, 2, 3, 4, 5, 10]; // 10 is bigger  than 5
+
+for (i = 0; i < 5; i++) {
+    console.log(a[i]); // when you run the file it won't print number 10 because it bigger than 5
+}
+
+setTimeout(function () {
+    process.exit();
+}, 500000);
