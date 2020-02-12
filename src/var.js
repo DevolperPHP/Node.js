8c56acf6d28d7754982fc8790457b5c3b2382795
DevolperPHP <devolperphp@gmail.com>
2020-02-12 05:38:03 -0800

    Update var.js

--- src/var.js
+++ src/var.js
@@ -1,1 +1,24 @@
+var profile = {
+    firstname: "Mohammed",
+    LastName: "Majid",
+    age: 26
+}
 
+var info = {
+    tall: "1.80 m",
+    FavFood: "HotDog",
+}
+
+
+function fav() {
+    console.log(info);
+}
+
+function dispaly() {
+
+    console.log(profile);
+
+}
+
+dispaly();
+fav();
