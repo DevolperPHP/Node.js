88ba0df124c75b7a46619d343717e0ba76cf2b6b
DevolperPHP <devolperphp@gmail.com>
2020-02-15 13:05:21 -0800

    Update server.js

--- src/Express/server.js
+++ src/Express/server.js
@@ -1,1 +1,13 @@
 
+const express = require('express') ;
+
+const app = express() ;
+
+app.use((req , res) =>{
+    res.json({
+        meassage : "Express working",
+        sucsess : 'true'
+    })
+})
+
+module.exports = app ;
