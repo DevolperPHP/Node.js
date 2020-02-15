b27bfc620f364bd295beca769b2bb62018c413d7
DevolperPHP <devolperphp@gmail.com>
2020-02-15 03:36:34 -0800

    Update result.js

--- src/ModuleAndExports/More_than_function/result.js
+++ src/ModuleAndExports/More_than_function/result.js
@@ -1,1 +1,17 @@
+/*
+This file have more than one function and I need to use all the function with another file
+so I will use another type of module and export
+*/
+function add(a,b){
+    console.log(a+b)
+}
 
+
+function squar(s){
+    console.log(s*s)
+}
+
+module.exports = {
+    add : add,
+    squar : squar
+}
