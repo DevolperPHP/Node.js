8215d473da1cd990d16395c9c00cfab9d86cc473
DevolperPHP <devolperphp@gmail.com>
2020-02-15 06:13:27 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
+const http = require('http') ;
 
+const server = http.createServer((req , res) => {
+    res.end("Hello")
+  })
+
+});
+
+
+server.listen(3000 , '127.0.0.1' , ()=>{
+  console.log('Server is running ....')
+});
