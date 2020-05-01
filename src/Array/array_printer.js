ac26a8ddc59c55c9f732ed451565fb0850ec1e3c
DevolperPHP <devolperphp@gmail.com>
2020-05-01 10:42:33 -0700

    Create array_printer.js

---
+++ src/Array/array_printer.js
@@ -0,0 +1,7 @@
+/* if we use console.log(array); the result will be [items] but if i want
+   to print the items without [] like Ali , Mohammed , Ahmed here I should use foreach */
+   
+   var arr3 = [ 'Mohammed', 'Nour', 'Ahmed' ];
+for (var i = 0; i < arr3.length; i++) {
+    console.log(arr3[i]);
+}
