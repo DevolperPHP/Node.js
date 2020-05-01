399a9348c0e916b96c1431af4082334338ceae4b
DevolperPHP <devolperphp@gmail.com>
2020-05-01 10:31:51 -0700

    Update array.js

--- src/Array/array.js
+++ src/Array/array.js
@@ -1,1 +1,9 @@
+// First we should make the array by using var *array_name* = [items]
 
+var name = ['Ali' , 'Mohammed' , 'Abd'];
+
+// Now we crated the array let's print it
+
+// to print it we use console.log(*array_name*);
+
+console.log(name);
