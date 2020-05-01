4ebc29a34368743eede3e747e016b7d58ef48781
DevolperPHP <devolperphp@gmail.com>
2020-05-01 11:03:01 -0700

    Create item_placement.js

---
+++ src/Array/item_placement.js
@@ -0,0 +1,8 @@
+/* all the items in the array have a ID and it start with number 0
+   and you can set the item place by using the id */
+   
+   // this is the array
+   var arr3 = [ 'ahmed', 'ali', 'sarah' ];
+   // lets add mohammed on a diffrent place
+arr3[20] = "mohammed";
+console.log(arr3);
