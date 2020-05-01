8f4148991cb23916224fe7a4d6dfefea613a50d8
DevolperPHP <devolperphp@gmail.com>
2020-05-01 10:54:13 -0700

    Create add_items.js

---
+++ src/Array/add_items.js
@@ -0,0 +1,11 @@
+// let's make array
+
+var arr3 = [ 'nour', 'malak', 'ali' ];
+// lets add mohammed
+arr3.push("mohammed");
+// lets print the array to make sure that mohammed added to the array
+console.log(arr3);
+// lets add sarah with another way
+arr3[arr3.length] = "fat";
+// lets print the array
+console.log(arr3);
