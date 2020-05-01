35cf19c52276aa729452d09ad4ffeb7c96a05fec
DevolperPHP <devolperphp@gmail.com>
2020-05-01 10:38:35 -0700

    Create length.js

---
+++ src/Array/length.js
@@ -0,0 +1,7 @@
+// What is length?
+/* we use length to see how many items inside the array like ['mohammed' , 'ali']
+   in this array theres tow items but if we have more than 1000 item we use length to see how many items in the array */
+   
+   var arr = ['mohammed' , 'njm' , 'nour' , 'malak'];
+   
+   console.log(arr.length);
