e922bdc6c0dc58aafea16f59a8664a02243cf4e9
DevolperPHP <devolperphp@gmail.com>
2020-06-01 19:49:33 +0300

    Create for.js

---
+++ src/Ejs/for.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><%= title %></title>
+    <link rel='stylesheet' href='/stylesheets/style.css' />
+  </head>
+  <body>
+  <% for(var i = 0; i <= 10; i++){ %>
+  <br><%= i %>
+  <%}%>
+  </body>
+</html>
